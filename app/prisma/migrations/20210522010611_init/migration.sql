d15383a765ed62af6fb5026ded450560ea4d12e6
entsuka70 <entsuka70@gmail.com>
2021-05-22 10:33:19 +0900

    add: Prisma初期設定

---
+++ app/prisma/migrations/20210522010611_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "setupData" (
+    "id" TEXT NOT NULL,
+    "required" BOOLEAN NOT NULL,
+    "number" SMALLINT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
---
+++ app/testPrisma.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    await prisma.setupData.create({
+        data: {
+            id: 'setup',
+            required: false,
+            number: 1
+        }
+    })
+
+    const allDatas = await prisma.setupData.findMany()
+    console.log(allDatas)
+    console.dir(allDatas, {depth: null})
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
