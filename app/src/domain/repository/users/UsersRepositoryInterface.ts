f3e7e0c5af36ea1cd4387b1820bc85b5fd9cab46
entsuka70 <entsuka70@gmail.com>
2021-05-31 09:44:31 +0900

    add: UserRepository関連のCRUD処理

--- app/src/domain/repository/users/UsersRepositoryInterface.ts
+++ app/src/domain/repository/users/UsersRepositoryInterface.ts
@@ -1,11 +1,13 @@
 import Team from 'domain/entity/users/team';
 import Pair from 'domain/entity/users/pair';
 import User from 'domain/entity/users/user';
+import { NodeWorker } from 'inspector';
 
 export default interface UsersRepositoryInterface {
+    find(id: number): Promise<object>;
     findAll(): Promise<User[]>|Promise<Pair[]>|Promise<Team[]>;
     findById(id: number): Promise<User>|Promise<Pair>|Promise<Team>;
-    create(entity: User|Pair|Team): Promise<User>|Promise<Pair>|Promise<Team>;
-    update(entity: User|Pair|Team): Promise<User>|Promise<Pair>|Promise<Team>;
-    delete(id: number): Promise<User>|Promise<Pair>|Promise<Team>;
+    create(data: {pair_id: number|null, user_name: string, email: string, belong_id: number|null} ): Promise<void>;
+    update(data: {id: number, pair_id: number, user_name: string, email: string, belong_id: number}): Promise<void>;
+    delete(id: number): Promise<void>;
 }
--- app/src/infra/repository/UserRepository.ts
+++ app/src/infra/repository/UserRepository.ts
@@ -10,36 +10,85 @@ export default class UserRepository implements UsersRepositoryInterface {
         this.prisma = prisma;
     }
 
+    public async find(user_id: number): Promise<object> {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                id: user_id
+            },
+        });
+        if (user == null) {
+            throw new Error(`Not Found User(user_id : ${user_id}).`)
+        }
+        return user;
+    }
+
     public async findAll(): Promise<User[]> {
         const all_users = await this.prisma.user.findMany({
-            include: {belong: true},
-        })
-        console.log(all_users);
-
-        const entity: User[] = all_users.map(
+            include: {
+                belong: true
+            },
+        });
+        const entities: User[] = all_users.map(
             (model): User => {
                 return new User(
                     model.id, model.user_name, model.email, new BelongsValueObject(model.belong.belong)
                 )
             }
         )
+        return entities;
+    }
+
+    public async findById(user_id: number): Promise<User> {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                id: user_id
+            },
+            include: {
+                belong: true
+            }
+        });
+        if (user == null) {
+            throw new Error(`Not Found User(user_id : ${user_id}).`)
+        }
+        const entity = new User(user.id, user.user_name, user.email, new BelongsValueObject(user.belong.belong));
         return entity;
     }
 
-    public async findById(): Promise<User> {
-        return new User(1, 'test', 'test@mail', new BelongsValueObject());
+    public async create(data: {pair_id:number|null, user_name:string, email:string, belong_id:number|null}): Promise<void> {
+        await this.prisma.user.create({
+            data: {
+                pair_id: data.pair_id  ?? 1,
+                user_name: data.user_name,
+                email: data.email,
+                belong_id: data.belong_id ?? 1,
+            }
+        });
+        return;
     }
 
-    public async create(): Promise<User> {
-        return new User(1, 'test', 'test@mail', new BelongsValueObject());
+    public async update(data: {id: number, pair_id: number, user_name: string, email: string, belong_id: number}): Promise<void> {
+        await this.prisma.user.update({
+            where: {
+                id: data.id,
+            },
+            data: {
+                pair_id: data.pair_id,
+                user_name: data.user_name,
+                email: data.email,
+                belong_id: data.belong_id,
+                updated_at: new Date(),
+            }
+        });
+        return;
     }
 
-    public async update(): Promise<User> {
-        return new User(1, 'test', 'test@mail', new BelongsValueObject());
-    }
-
-    public async delete(): Promise<User> {
-        return new User(1, 'test', 'test@mail', new BelongsValueObject());
+    public async delete(user_id: number): Promise<void> {
+        await this.prisma.user.delete({
+            where: {
+                id: user_id
+            }
+        });
+        return;
     }
     
 }
