7a6e33f551d013683540a207f00dad160e4b9a09
entsuka70 <entsuka70@gmail.com>
2021-06-06 12:18:32 +0900

    fix: POST処理の正常動作修正

--- app/src/presentation/controller/UserController.ts
+++ app/src/presentation/controller/UserController.ts
@@ -3,20 +3,13 @@ import { PrismaClient } from ".prisma/client";
 import UserApplication from "app/application/users/UserApplication";
 import UserFactory from "infra/factory/users/user/UserFactory";
 import UserRepository from "infra/repository/UserRepository";
-import bodyParser from 'body-parser';
-
-const app = express();
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
-// const jsonParser = bodyParser.json();
-app.use(express.json());
 
 // ユーザー一覧取得
 exports.view = async function(req: express.Request, res: express.Response) {
     const prisma = new PrismaClient();
     const userFactory = new UserFactory();
     const userRepository = new UserRepository(prisma);
-    const userApplication = new UserApplication(userRepository, userFactory); // インターフェイスを継承したuserRepositoryだから引数として利用できるのか？
+    const userApplication = new UserApplication(userRepository, userFactory);
     const userAll = await userApplication.findUserAll();
     res.set({
         'content-type': 'application/json',
--- app/src/server.ts
+++ app/src/server.ts
@@ -2,6 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// express4.xx以降で以下を同梱
+// https://expressjs.com/ja/api.html
+// This is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(express.json());
+
 /* 5000番ポートで待ち受け */
 const server = app.listen(5000, function () {
 console.log(`Node.js is listening to PORT: ${server.address().port}`);
