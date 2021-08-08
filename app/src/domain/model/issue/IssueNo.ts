a48a9702b91a57dbf2b81ffa5b83a43cf647085c
entsuka70 <entsuka70@gmail.com>
2021-08-08 10:07:29 +0900

    add: issue_noエンティティ追加

---
+++ app/src/domain/model/issue/IssueNo.ts
@@ -0,0 +1,16 @@
+export default class IssueNo {
+    private issueNo: number;
+
+    static ISSUE_NO_MATCHER = /^[0-9]{1,3}$/;
+
+    constructor(issueNo: number) {
+        if (issueNo && !IssueNo.ISSUE_NO_MATCHER.test(String(issueNo))) {
+            throw new Error('Do not match TeamName FORMAT')
+        }
+        this.issueNo = issueNo
+    }
+
+    public get(): number {
+        return this.issueNo
+    }
+}
