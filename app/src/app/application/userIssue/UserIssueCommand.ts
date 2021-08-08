bccd575cc56a6f0fa28c3902cbbc31fe2571f09c
entsuka70 <entsuka70@gmail.com>
2021-08-08 18:28:13 +0900

    add: UserIssueCommand追加

---
+++ app/src/app/application/userIssue/UserIssueCommand.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+export default class UserIssueCommand {
+    public id: string;
+    public issue_id: string;
+    public user_id: string;
+    public progress: number;
+
+    constructor(req: express.Request) {
+        this.id = req.params.id || req.body.id
+        this.issue_id = req.body.issue_id;
+        this.user_id = req.body.user_id;
+        this.progress = req.body.progress;
+    }
+
+    public getId() {
+        return this.id;
+    }
+}
