73b3ce54038cfc19a81f64f882a627fa3c284c22
entsuka70 <entsuka70@gmail.com>
2021-08-08 12:15:25 +0900

    add: IssueControllerの追加

--- app/src/presentation/controller/IssueController.ts
+++ app/src/presentation/controller/IssueController.ts
@@ -1,37 +1,20 @@
 import express from 'express';
 import { PrismaClient } from ".prisma/client";
-import IssueApplication from "app/application/issues/IssueApplication";
-import IssueFactory from "domain/factory/IssueFactory";
+import IssueApplication from "app/application/issue/IssueApplication";
 import IssueRepository from "infra/repository/IssueRepository";
 
-/**
- * 課題プレゼンテーション層
- * 課題の更新
- * method: POST
- * URI: issue/:id
- * data: {object}
- * @param {express.Request} req
- * @param {express.Response} res
- */
-exports.update = async function (req: express.Request, res: express.Response) {
+// 課題一覧取得
+exports.view = async function (req: express.Request, res: express.Response) {
+    const prisma = new PrismaClient();
+    const issueRepository = new IssueRepository(prisma);
+    const issueApplication = new IssueApplication(issueRepository);
     try {
-        const prisma = new PrismaClient();
-        const issueFactory = new IssueFactory();
-        const issueRepository = new IssueRepository(prisma);
-        const issueApplication = new IssueApplication(issueRepository, issueFactory);
-
-        const data = {
-            'id': parseInt(req.params.id),
-            'issue_name': req.body.issue_name,
-            'issue_group_id': parseInt(req.body.issue_group_id),
-        };
-
-        await issueApplication.update(data);
+        const issues = await issueApplication.findAll();
         res.set({
-            'content-type': 'text/plain',
+            'content-type': 'application/json',
         });
-        res.status(201).send('Issue Update success');
+        return res.status(200).json(issues);
     } catch (e) {
-        res.status(400).send(e.message);
+        return res.status(400).send(`Error: User View (${e.message})`);
     }
 }
