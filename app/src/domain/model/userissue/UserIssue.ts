b72b96afb500f87349eefece5e161828dfaf1bbc
entsuka70 <entsuka70@gmail.com>
2021-08-08 11:20:11 +0900

    add: UserIssueエンティティ追加

---
+++ app/src/domain/model/userissue/UserIssue.ts
@@ -0,0 +1,63 @@
+import UserIssueId from "./UserIssueId";
+import UserId from "domain/model/user/UserId";
+import IssueId from "domain/model/issue/IssueId";
+import UserIssueProgress from "./UserIssueProgress";
+
+type Props = {
+    id: UserIssueId
+    user_id: UserId
+    issue_id: IssueId
+    progress: UserIssueProgress
+}
+
+export type UserIssueProps = Required<Props>
+
+export default class UserIssue {
+    private id: UserIssueId;
+    private user_id: UserId;
+    private issue_id: IssueId;
+    private progress: UserIssueProgress;
+
+    constructor(props: UserIssueProps) {
+        const { id, user_id, issue_id, progress } = props;
+        this.id = id;
+        this.user_id = user_id;
+        this.issue_id = issue_id;
+        this.progress = progress;
+    }
+
+    public getAllProperties() {
+        return {
+            id: this.id.get(),
+            user_id: this.user_id.get(),
+            issue_id: this.issue_id.get(),
+            progress: this.progress.get()
+        }
+    }
+
+    public getId() {
+        return this.id.get()
+    }
+
+    public getUserId() {
+        return this.user_id.get()
+    }
+
+    public getIssueId() {
+        return this.issue_id.get()
+    }
+
+    public getProgress() {
+        return this.progress.get()
+    }
+
+    public changeProgress(progress: UserIssueProgress, requestUser: string) {
+        if (progress.get() == UserIssueProgress.PROGRESS_COMPLETE) {
+            throw new Error('Can not change progress because the progress has already completed.')
+        }
+        if (requestUser !== this.user_id.get()) {
+            throw new Error('Can not change progress because issue owner is not correct.')
+        }
+        this.progress = progress;
+    }
+}
