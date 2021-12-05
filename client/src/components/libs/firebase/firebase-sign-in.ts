b015ccbc29199f8a434b338c0ef3a43921398d0b
qara70 <entsuka70@gmail.com>
2021-12-05 18:05:22 +0900

    fix: eslintで修正されたsign-inとsign-up

--- client/src/components/libs/firebase/firebase-sign-in.ts
+++ client/src/components/libs/firebase/firebase-sign-in.ts
@@ -1,11 +1,14 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./firebase-auth";
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from './firebase-auth'
 
 interface SignInProps {
   email: string
   password: string
 }
 
-export const signIn = async ({email, password}: SignInProps): Promise<void> => {
+export const signIn = async ({
+  email,
+  password,
+}: SignInProps): Promise<void> => {
   await signInWithEmailAndPassword(auth, email, password)
 }
--- client/src/components/libs/firebase/firebase-sign-up.ts
+++ client/src/components/libs/firebase/firebase-sign-up.ts
@@ -1,11 +1,14 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./firebase-auth"
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from './firebase-auth'
 
 interface SingUpProps {
   email: string
   password: string
 }
 
-export const signUp = async ({email, password}: SingUpProps): Promise<void> => {
+export const signUp = async ({
+  email,
+  password,
+}: SingUpProps): Promise<void> => {
   await createUserWithEmailAndPassword(auth, email, password)
 }
