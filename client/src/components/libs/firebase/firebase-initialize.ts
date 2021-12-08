553ce9b3fc69a5573eb16964d635c1e939fd3ec8
qara70 <entsuka70@gmail.com>
2021-12-08 21:27:27 +0900

    fix: firebase初期化を修正

--- client/src/components/libs/firebase/firebase-initialize.ts
+++ client/src/components/libs/firebase/firebase-initialize.ts
@@ -2,4 +2,3 @@ import firebase from 'firebase/compat/app'
 import { firebaseConfig } from './firebase-config'
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
-export default firebase
