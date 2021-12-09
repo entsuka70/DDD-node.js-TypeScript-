3cb6f02de8652b8ff1c1a66d007fbc65deece2f9
qara70 <entsuka70@gmail.com>
2021-12-10 08:40:09 +0900

    add: 簡易UIの作成

--- client/src/components/page/home/Home.tsx
+++ client/src/components/page/home/Home.tsx
@@ -1,5 +1,40 @@
 const Home = (): JSX.Element => {
-  return <div></div>
+  return (
+    <div>
+      <h1>リクエストホーム</h1>
+      <div>
+        <h2>ユーザーリスト</h2>
+        <button>ユーザーリスト取得</button>
+        <div>
+          <ul>
+            <li>ユーザーホゲ</li>
+            <li>ユーザーフガ</li>
+            <li>ユーザーホルツ</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h2>ペアリスト</h2>
+        <button>ペアリスト取得</button>
+        <div>
+          <ul>
+            <li>ペアホゲ</li>
+            <li>ペアフガ</li>
+            <li>ペアホルツ</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h2>チームリスト</h2>
+        <button>チームリスト取得</button>
+        <div>
+          <ul>
+            <li>チームはありません</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
