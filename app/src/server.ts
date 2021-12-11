61e9a30819267eaad6e565cb4ede718db0282144
qara70 <entsuka70@gmail.com>
2021-12-11 09:53:28 +0900

    add: サーバー側でCORSを解消するオリジンを設定

--- app/src/server.ts
+++ app/src/server.ts
@@ -5,12 +5,21 @@ import pair from './routes/pair';
 import team from './routes/team';
 import issue from './routes/issue';
 import userIssue from './routes/userIssue';
+import cors from 'cors';
+import e from 'cors';
 const app = express();
 // express4.xx以降で以下を同梱
 // https://expressjs.com/ja/api.html
 // This is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
+const corsOptions: e.CorsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/', user);
